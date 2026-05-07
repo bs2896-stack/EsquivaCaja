3e8066cfd6d083b847b89af52fc30e24c553f1dc
bs2896-stack <bs2896@est.ort.edu.ar>
2026-05-07 10:43:19 -0300

    Collision detector caja

---
+++ Assets/scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OncollisionEnter (Collision col)
+
+    {
+        if (col.gameObject.CompareTag("Caja"))
+        {
+           
+        }
+         Destroy(gameObject);
+
+
+
+    }
+}
